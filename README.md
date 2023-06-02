92dfc9078f0db63469b080e8a7e4f588ed296b28
Leigh Robinson <leighspost@hotmail.com>
2023-06-02 22:56:53 +0100

    added responsive navigation

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-Front End Developer portfolio website.
+# Front End Developer Portfolio Website
 
-This will be rebuilt with React.
+## Project Description
+This project aims to build a frontend developer portfolio website from scratch, providing an opportunity to gain experience in web development.
+
+## Contributing Guidelines
+This is a solo project, and contributions from others are not required. However, assistance may be requested when facing issues, but the overall development is being handled individually.
+
+## License
+Permission is granted to read the contents of this project for informational purposes only. Usage, copying, distribution, selling, or any other form of utilization beyond reading is strictly prohibited.
+
+## Dependencies
+- Google Fonts
+
+## Roadmap
+In the future, there are plans to rebuild this project using React.
--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+/* https://www.w3schools.com/howto/howto_js_topnav_responsive.asp */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    navbar.classList.remove("sticky");
+    x.className = "topnav";
   }
 }
